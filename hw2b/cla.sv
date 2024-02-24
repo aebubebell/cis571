b97369bce4c4bf94f2dfea4793d342b642a3a50c
aebubebell <72332181+aebubebell@users.noreply.github.com>
2024-02-24 02:45:47 -0500

    Update cla.sv

--- hw2b/cla.sv
+++ hw2b/cla.sv
@@ -82,6 +82,17 @@ wire [2:0] carry_mid;
 wire gen_out_mid, prop_out_mid;
 gp1 gp1_blocks[31:0] (.a(a), .b(b), .g(gen_signal), .p(propagate));
 
+generate
+    genvar i;
+    for (i = 0; i < 32; i = i + 1) begin : gp1_blocks
+        gp1 gp1_instance(
+            .a(a[i]),       // Connect to the ith bit of a
+            .b(b[i]),       // Connect to the ith bit of b
+            .g(gen_signal[i]), // Connect to the ith bit of gen_signal
+            .p(propagate[i]) // Connect to the ith bit of propagate
+        );
+    end
+endgenerate
 
 gp8 module1(.gin(generate[7:0]), .pin(propagate[7:0]), .cin(cin),
     .gout(generate_mid[0]), .pout(propagate_mid[0]), .cout(carry[6:0]));
