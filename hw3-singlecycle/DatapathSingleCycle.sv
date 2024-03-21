6a38e8776a14edc91d7738af4141cbb9502a5ded
aebubebell <72332181+aebubebell@users.noreply.github.com>
2024-03-21 13:36:33 -0400

    Update DatapathSingleCycle.sv

--- hw3-singlecycle/DatapathSingleCycle.sv
+++ hw3-singlecycle/DatapathSingleCycle.sv
@@ -156,7 +156,7 @@ module DatapathSingleCycle (
   wire insn_srai = insn_opcode == OpRegImm && insn_from_imem[14:12] == 3'b101 && insn_from_imem[31:25] == 7'b0100000;
 
   wire insn_add = insn_opcode == OpRegReg && insn_from_imem[14:12] == 3'b000 && insn_from_imem[31:25] == 7'd0;
-  wire insn_  = insn_opcode == OpRegReg && insn_from_imem[14:12] == 3'b000 && insn_from_imem[31:25] == 7'b0100000;
+  wire insn_sub  = insn_opcode == OpRegReg && insn_from_imem[14:12] == 3'b000 && insn_from_imem[31:25] == 7'b0100000;
   wire insn_sll = insn_opcode == OpRegReg && insn_from_imem[14:12] == 3'b001 && insn_from_imem[31:25] == 7'd0;
   wire insn_slt = insn_opcode == OpRegReg && insn_from_imem[14:12] == 3'b010 && insn_from_imem[31:25] == 7'd0;
   wire insn_sltu = insn_opcode == OpRegReg && insn_from_imem[14:12] == 3'b011 && insn_from_imem[31:25] == 7'd0;
@@ -265,7 +265,7 @@ module DatapathSingleCycle (
       OpLui: begin
         // TODO: start here by implementing lui
         rf_we = 1'b1;
-        rf_wdata = ;
+        rf_wdata = lui_imm;
       end
       OpRegImm: begin
         case (insn_funct3)
