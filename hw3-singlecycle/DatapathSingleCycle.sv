241641eab28be1d741a92c364bbe6f1ec9a66a76
aebubebell <72332181+aebubebell@users.noreply.github.com>
2024-03-27 08:47:11 -0400

    Update DatapathSingleCycle.sv

--- hw3-singlecycle/DatapathSingleCycle.sv
+++ hw3-singlecycle/DatapathSingleCycle.sv
@@ -347,8 +347,8 @@ module DatapathSingleCycle (
               // rf_we = 1'b1;
               // rf_wdata = ($signed(rs1_data) * $signed(rs2_data)) >> 32;
                 logic [63:0] multh_hld;
-              multh_hld = ($signed(data_rs1) * $signed(data_rs2));
-              rf_wdata = inter_mulh[63:32];
+                multh_hld = ($signed(rs1_data) * $signed(rs2_data));
+              rf_wdata = multh_hld[63:32];
             end else if (insn_mulhsu) begin
               // MULHSU
               rf_we = 1'b1;
